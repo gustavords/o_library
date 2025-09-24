dd2fc5592d0d4f020714e356aad19889bb6137da
gustavords <22622146+gustavords@users.noreply.github.com>
2025-09-24 17:55:07 -0400

    Create modalForm(), display new book onto display

--- js/script.js
+++ js/script.js
@@ -14,18 +14,53 @@ function addBookToLibrary ( title, author )
     myLibrary.push( book );
 }
 
-addBookToLibrary( `Foundation`, `Isaac Asimov` );
-addBookToLibrary( `Lord of the Rings`, `J.R.R. Tolkien` );
-addBookToLibrary( `Harry Potter`, `J.K. Rowling` );
-
 function displayLibrary ()
 {
     const display = document.querySelector( `.library_display` );
 
+    //clean display
+    while ( display.firstChild )
+    {
+        display.removeChild( display.lastChild );
+    }
+
     myLibrary.forEach( ( book ) =>
     {
-        display.textContent += `${book.description()}`
+        const bookCard = document.createElement( `p` );
+        display.appendChild( bookCard );
+        bookCard.textContent = `${ book.description() }`
     } );
 }
 
-displayLibrary();
+function modalForm ()
+{
+    const addBook_btn = document.querySelector( `#add_book` );
+    const dialog = document.querySelector( `#dialog` );
+    const confirmBtn = document.querySelector( `#confirmBtn` )
+    const b_title = document.querySelector( `#b_title` );
+    const b_author = document.querySelector( `#b_author` );
+
+    addBook_btn.addEventListener( `click`, () =>
+    {
+        dialog.showModal();
+    } );
+    
+    confirmBtn.addEventListener( `click`, ( e ) =>
+    {
+        e.preventDefault(); // We don't want to submit this fake form
+        dialog.close();
+    } );
+
+    dialog.addEventListener( "close", () =>
+    {
+        if ( b_title.value == `` || b_author.value == `` ) { return };
+        addBookToLibrary( b_title.value, b_author.value );
+        displayLibrary();
+    } )
+}
+
+addBookToLibrary( `Foundation`, `Isaac Asimov` );
+addBookToLibrary( `Lord of the Rings`, `J.R.R. Tolkien` );
+addBookToLibrary( `Harry Potter`, `J.K. Rowling` );
+
+modalForm();
